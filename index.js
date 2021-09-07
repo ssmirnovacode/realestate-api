6fabcf6f6eca6dd82eed485db9dc1df43ce4ce8e
ssmirnova <svetlana.smirnova213@gmail.com>
2021-09-07 13:23:31 +0200

    Origin header fixed

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 //CORS policy
 app.use((req,res,next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://ssmirnovacode.github.io/my-realestate'); //, http://localhost:3000
+    res.setHeader('Access-Control-Allow-Origin', 'https://ssmirnovacode.github.io/my-realestate'); //, http://localhost:3000
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
